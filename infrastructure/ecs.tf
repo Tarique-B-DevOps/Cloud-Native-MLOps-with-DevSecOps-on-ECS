7128aacdac5fe8548b65429e64414f3c229033da
Tarique-B-DevOps <tariqueatwork@gmail.com>
2025-09-29 17:09:05 +0000

    log config added

--- infrastructure/ecs.tf
+++ infrastructure/ecs.tf
@@ -1,3 +1,4 @@
+# ECS Cluster
 resource "aws_ecs_cluster" "cluster" {
   name = local.resource_prefix
 }
@@ -9,13 +10,14 @@ resource "aws_ecs_cluster_capacity_providers" "fargate" {
   ]
 }
 
+# Task Definition
 resource "aws_ecs_task_definition" "def" {
   family                   = local.resource_prefix
   execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
-  cpu                      = "256"
-  memory                   = "512"
+  cpu                      = "1024"
+  memory                   = "2048"
 
   container_definitions = jsonencode([
     {
@@ -28,6 +30,14 @@ resource "aws_ecs_task_definition" "def" {
           hostPort      = var.model_port
         }
       ]
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          awslogs-group         = "/ecs/${local.resource_prefix}"
+          awslogs-region        = var.region
+          awslogs-stream-prefix = "ecs"
+        }
+      }
     }
   ])
 }
