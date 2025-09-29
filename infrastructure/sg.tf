a79b2dc3615251dfa533f8b3312116de52250e4d
Tarique-B-DevOps <tariqueatwork@gmail.com>
2025-09-29 17:34:11 +0000

    ecs service sg updated

--- infrastructure/sg.tf
+++ infrastructure/sg.tf
@@ -21,3 +21,26 @@ resource "aws_security_group" "alb_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+# ECS Service SG
+resource "aws_security_group" "ecs_sg" {
+  name        = "${local.resource_prefix}-ecs-sg"
+  description = "Allow inbound from ALB"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    description     = "Allow traffic from ALB"
+    from_port       = var.model_port
+    to_port         = var.model_port
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb_sg.id]
+  }
+
+  egress {
+    description = "Allow all outbound"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
