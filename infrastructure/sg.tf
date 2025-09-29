34762666f6b4c33f083320569291ee8060100a83
Tarique-B-DevOps <tariqueatwork@gmail.com>
2025-09-29 17:53:22 +0000

    fixes alb sg port

--- infrastructure/sg.tf
+++ infrastructure/sg.tf
@@ -2,13 +2,13 @@
 # ALB SG
 resource "aws_security_group" "alb_sg" {
   name        = "${local.resource_prefix}-alb-sg"
-  description = "Allow inbound for ALB"
+  description = "Allow HTTP inbound for ALB"
   vpc_id      = module.vpc.vpc_id
 
   ingress {
     description = "Allow Model Port"
-    from_port   = var.model_port
-    to_port     = var.model_port
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
